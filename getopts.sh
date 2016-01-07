78a851d9deb23ab89a5b5f7153a09f34a730654e
b4b4r07 <b4b4r07@gmail.com>
2016-01-08 00:20:14 +0900

    Repalce $argv with $@ in getopts.sh

--- getopts.sh
+++ getopts.sh
@@ -3,7 +3,7 @@ function getopts () {
         return 1
     fi
 
-    printf "%s\n" $argv | sed -E '
+    printf "%s\n" $@ | sed -E '
         s/^-([A-Za-z]+)/- \1 /
         s/^--([A-Za-z0-9_-]+)(!?)=?(.*)/-- \1 \3 \2 /' | awk -f getopts.awk
 }
